151a9b64e1a6d24117163a0c244c562b7d54d618
Bruce LANE <bruce@batchass.fr>
2016-11-10 20:40:20 +0100

    winston log to file, to console colorized

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict'
 
-var nodemon = require('nodemon'),
-winston = require('winston'),
-http = require('http').createServer(),
-wss = require('./src/ws.js'),
-port = process.env.port || 8088;
+var nodemon = require('nodemon')
+  , winston = require('winston')
+  , http = require('http').createServer()
+  , wss = require('./src/ws.js')
+  , port = process.env.port || 8088;
 
 http.listen(port, function() {
+    winston.configure({
+    transports: [
+        new (winston.transports.Console)({ colorize: true }),
+        new (winston.transports.File)({ filename: 'netdromm.log' })
+    ]
+  });
     winston.info('Application server running!');
     new wss(this, winston, function(err){
         if (!err) winston.info('Websocket listening on port ' + port);
