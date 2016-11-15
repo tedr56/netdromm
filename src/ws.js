fa82684450fb9afc57bdb40ed9ce64969a2f059b
jarpi <josep.arpi@gmail.com>
2016-11-15 20:09:10 +0100

    Fix for #7 Major refactor Initialization of the app and initialization of the websocket server has been refactored using promises.

--- src/ws.js
+++ src/ws.js
@@ -2,28 +2,46 @@
 var uuid = require('uuid');
 var WebSocketServer = require('ws').Server;
 
-function wsCtrl ( httpListener, logger, cb ) {
+function wsCtrl ( httpListener, logger ) {
     this.logger = logger;
     this.wss = new WebSocketServer({ server: httpListener });
-    var that = this;
-    this.wss.broadcast = function(msg, sender) {
-        that.wss.clients.forEach(function(client){
-            if (client.id !== sender) client.send(msg) & logger.info('Message sent:' + msg + ' from ' + sender + ' to ' + client.id);
-        });
-    }
-    this.wss.on('connection', function(socket){
-      socket.id = uuid.v4();
-      // to try if behind a proxy: logger.info('user connected' + socket.upgradeReq.headers['x-forwarded-for']);
-      logger.info('user connected: ' + socket.upgradeReq.connection.remoteAddress);
-      socket.on('disconnect', function(){
-        logger.info('user disconnected: ' + socket.upgradeReq.connection.remoteAddress);
-      });
-      socket.on('message', function(msg){
-        logger.info('Message received: ' + msg + ' from ' + socket.upgradeReq.connection.remoteAddress);
-        that.wss.broadcast(msg, socket.id);
-      });
-    });
-    return cb(null);
+    // var that = this;
+    return Promise.resolve(this);
 }
 
-module.exports = wsCtrl;
+function addBroadcastPolyfill( instance ) {
+    return Promise.resolve()
+    .then(function(){
+        instance.wss.broadcast = function(msg, sender) {
+            instance.wss.clients.forEach(function(client){
+                if (client.id !== sender) client.send(msg) & instance.logger.info('Message sent:' + msg + ' from ' + sender + ' to ' + client.id);
+            });
+        }
+        return instance;
+    })
+}
+
+function addWsEvtListeners( instance ) {
+    return Promise.resolve()
+    .then(function(){
+         instance.wss.on('connection', function(socket){
+          socket.id = uuid.v4();
+          // to try if behind a proxy: logger.info('user connected' + socket.upgradeReq.headers['x-forwarded-for']);
+          instance.logger.info('user connected: ' + socket.upgradeReq.connection.remoteAddress);
+          socket.on('disconnect', function(){
+            instance.logger.info('user disconnected: ' + socket.upgradeReq.connection.remoteAddress);
+          });
+          socket.on('message', function(msg){
+            instance.logger.info('Message received: ' + msg + ' from ' + socket.upgradeReq.connection.remoteAddress);
+            instance.wss.broadcast(msg, socket.id);
+          });
+        });
+        return instance;
+    });
+}
+
+module.exports = function( httpListener, logger ) {
+    return new wsCtrl( httpListener, logger )
+    .then(addBroadcastPolyfill)
+    .then(addWsEvtListeners);
+}
